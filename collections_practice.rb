c78c2f088abe50b8fca0987378118917215cfd70
Matoya Williams <matoyawilliams@yahoo.com>
2020-01-30 04:42:41 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,22 @@
+array = [2,4,6,5,0,1,3]
 
+array.sort do |a,b|
+  a<=>b 
+end
+
+array.sort do |b,a|
+  b<=>a 
+end 
+
+array_char = ["apple", "cat", "fish", "zebra", "goat"]
+array_char.sort do |a,b|
+  a<=>b 
+end 
+
+swap_elements_from_to(["love","live","laugh"],0,2)
+end 
+
+reverse_array = (["love","live","laugh"],2,1,0)
+end 
+
+find_a = [""]
